e986c8e8ade766d1688d404ef83add122f891397
evajuma <evansjuma1e@gmail.com>
2026-02-07 00:57:11 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(firstword("         Juma is likes playing video games"))
+}
+
+func firstword(str string) string {
+	i := 0
+	for i < len(str) && str[i] == ' ' {
+		i++
+	}
+
+	start := i
+
+	for i < len(str) && str[i] != ' ' {
+		i++
+	}
+	return str[start:i]
+}
