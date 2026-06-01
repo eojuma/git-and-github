369a287452a5ecddbc2d0c1a602c40dfd37edd0e
evajuma <evansjuma1e@gmail.com>
2026-06-01 05:47:04 +0300

    fix: restore the tracked files that was lost

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(firstword("         Juma likes playing video games"))
+	fmt.Println(firstword("         Juma  likes playing video games"))
+	fmt.Println(firstword("  hello!       Juma  likes playing video games"))
+}
+// this function finds the fast word of any sring passed to it
+func firstword(str string) string {
+	// this will ensure that leading spaces are well taken care of
+	// the iteration will occur until the character is not a space.
+	i := 0
+	for i < len(str) && str[i] == ' ' {
+		i++
+	}
+//  at this point we now tell the iterator that we are beginnig to encounter character which are not spaces
+	start := i
+
+	for i < len(str) && str[i] != ' ' {
+		i++
+	}
+
+
+	// it will print characters immediately after  the last space character to the last character before the next closest space character
+
+
+
+	return str[start:i]
+}
+// tihs was made simpler
